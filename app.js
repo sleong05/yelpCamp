45d0e4b0f222f140195170531bf33c88981d6d43
Sam <samuelleong05@gmail.com>
2025-06-17 16:07:34 -0400

    Adjust cookies for render

--- app.js
+++ app.js
@@ -111,6 +111,11 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 24 * 7,
     }
 }
+
+if (process.env.NODE_ENV === 'production') {
+    app.set('trust proxy', 1);
+}
+
 app.use(session(sessionConfig))
 
 app.use(passport.initialize())
