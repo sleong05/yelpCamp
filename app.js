dc8c74ec013f97dc717da75a4dac49802ece24e7
Sam <samuelleong05@gmail.com>
2025-06-17 15:35:26 -0400

    Adjusted secret

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ const store = MongoStore.create({
     mongoUrl: dbUrl,
     touchAfter: 24 * 60 * 60,
     crypto: {
-        secret: 'thisshouldbeabettersecret!'
+        secret: process.env.SECRET,
     }
 });
 
@@ -101,12 +101,12 @@ store.on("error", function (e) {
 const sessionConfig = {
     store,
     name: "session",
-    secret: 'thisshouldbeabettersecret',
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        secure: true,
+        secure: process.env.NODE_ENV === 'production',
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7,
     }
